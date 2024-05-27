464ed8fbd5ab8279512d868c539e203e2c13960f
william-wv <140337500+william-wv@users.noreply.github.com>
2024-05-27 20:05:47 -0300

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -5,6 +5,12 @@ export const dialogueData = {
 
   placa2: 'Este infografico trata-se sobre a como podemos possuir uma concientização ambiental melhor',
 
+  placa3: 'O crescimento das emissões de gases de efeito estufa contribui para as mudanças climáticas e agrava o problema do aquecimento global. A má qualidade do ar representa riscos graves para a saúde humana.',
+
+  placa4: 'Ter atitudes sustentáveis significa estar atento às necessidades do meio ambiente e fazer escolhas que não sejam prejudiciais ao planeta. Como por exemplo isso pode significar a compra de uma garrafa de água de vidro ou cerâmica em vez de garrafas de plástico descartáveis.',
+ 
+  placa5: '',
+  
   
   exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
 };
