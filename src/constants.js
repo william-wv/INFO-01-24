62b91f0e114a57e6bfaaa2cb391d8676d4efdab5
william-wv <140337500+william-wv@users.noreply.github.com>
2024-05-27 19:45:44 -0300

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -3,5 +3,8 @@ export const scaleFactor = 2;
 export const dialogueData = {
   placa1: 'Bem vindo ao inforgŕafico da turma de front2 do curso de TSI, UTFPR Toledo PR. Ande a vontade',
 
+  placa2: 'Este infografico trata-se sobre a como podemos possuir uma concientização ambiental melhor',
+
+  
   exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
 };
